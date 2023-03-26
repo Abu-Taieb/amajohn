3a49dbe06531db1e6c4bf04aaa5a51f8be34121a
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-03-27 00:08:14 +0600

    Header Design

---
+++ src/component/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+import logo from "../images/Logo.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={logo} alt="logo" />
+      <div className="headerNav">
+        <a href="shop">Shop</a>
+        <a href="order">Order</a>
+        <a href="inventory">Inventory</a>
+        <a href="login">Log In</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
