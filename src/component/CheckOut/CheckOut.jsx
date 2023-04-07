ac1719a91dda9c003ca2fa260aa789f71d360c34
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-04-07 15:09:11 +0600

    Shop, Order Checkout Page Successfully Design Complete

--- src/component/CheckOut/CheckOut.jsx
+++ src/component/CheckOut/CheckOut.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import "./CheckOut.css";
 
 const CheckOut = () => {
-    return (
-        <div>
-            <h3>Check Out Page</h3>
+  return (
+    <>
+      <div className="checkOut-container">
+        <h3>Check Out Page</h3>
+        <div className="checkOut-details">
+          <div className="checkOut-left">
+            <h4>Your Bank Account Details</h4>
+            <input type="text" className="" placeholder="Enter your bank information" />
+          </div>
+          <div className="checkOut-right">
+            <h4>Credit Card Details</h4>
+            <input type="number" className="" placeholder="Enter your card information" />
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default CheckOut;
--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-import './Product.css';
-
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import "./Product.css";
 
 const Product = (props) => {
-    const showData = props?.product;
-    const addToCart = props.addToCart;
+  const showData = props?.product;
+  const addToCart = props.addToCart;
 
-    return (
-        <div className='product'>
-            <img src={showData?.img} alt="" />
-            <h3>{showData?.name}</h3>
-            <h4>Price : ${showData?.price}</h4>
-            <p>Manufacturer : {showData?.seller}</p>
-            <p>Rating : {showData?.ratings} start</p>
-            <button onClick={() => addToCart(showData)} className='addToCart'>Add to Cart <FontAwesomeIcon icon={faCartShopping} />
-            </button>
-        </div>
-    );
+  return (
+    <div className="product">
+      <img src={showData?.img} alt="" />
+      <h3>{showData?.name}</h3>
+      <h4>Price : ${showData?.price}</h4>
+      <p>Manufacturer : {showData?.seller}</p>
+      <p>Rating : {showData?.ratings} start</p>
+      <div className="addToCartButton">
+        <button onClick={() => addToCart(showData)} className="addToCart">
+          Add to Cart <FontAwesomeIcon icon={faCartShopping} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
